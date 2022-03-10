15b2781498c58191dca1df0cd5b1f1a7dd2579ee
Le Hoang Trong <langtupro0456@gmail.com>
2022-03-10 22:14:59 +0700

    Add file React Hooks

---
+++ src/ReactHooks/Theory.jsx
@@ -0,0 +1,6 @@
+// Học xong khóa Master Reactjs này chúng ta có thể làm junior vì chúng ta đã có tiếp cận với dự án trong khóa rồi
+
+// React functional component thì không sử dụng state props của class components
+// Hooks ra đời cho phép chúng ta có thể sử dụng 2 khái niệm về state và lifeCycle này trong functional component, giúp code clean hơn và ngắn hơn.
+
+// Nên Hooks nó sẽ dùng nhiều hơn trong các dự án bây giờ
