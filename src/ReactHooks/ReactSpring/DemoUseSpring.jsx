c7cc7a4ef866a31e0a4295683381cc7b5c91660b
Le Hoang Trong <langtupro0456@gmail.com>
2022-03-14 17:43:06 +0700

    Study with React Spring

--- src/ReactHooks/ReactSpring/DemoUseSpring.jsx
+++ src/ReactHooks/ReactSpring/DemoUseSpring.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import {useSpring, animated} from 'react-spring'
+import React from 'react';
+import { useSpring, animated } from 'react-spring';
 
 const DemoUseSpring = (props) => {
+  // Đó là tìm hiểu về
+  const propsAnimation = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    config: { duration: 2000 },
+  });
 
-    const propsAnimation = useSpring({to: {opacity: 1},  from: {opacity: 0} })
+  const propsAnimation1 = useSpring({
+    to: { color_text: 'red' },
+    from: { color_text: 'blue' },
+    config: { duration: 2000 },
+  });
   return (
-    <animated.div style={propsAnimation}>
-        I will fade in
+    <animated.div
+      style={{
+        color: propsAnimation1.color_text,
+      }}
+    >
+      I will fade in
     </animated.div>
-  )
-}
+  );
+};
 
-export default DemoUseSpring
+export default DemoUseSpring;
--- src/ReactHooks/ReactSpring/Theory.jsx
+++ src/ReactHooks/ReactSpring/Theory.jsx
@@ -2,17 +2,17 @@
 /**
  * Thì để tạo ra animation thì bạn chỉ cần sử dụng css để thay đổi thuộc tính của phần tử kết hợp với thuộc tính transition. Nó có thể giúp chúng ta thực hiện các animation cần thiết như fadeIn, fadeOut, slideIn, slideOut\
  * - Sử dụng CSS chỉ gặp khó khăn khi animation của bạn quá phức tạp, kết hợp nhiều bước hoặc cần tính toan dựa trên tương tác c ủa người dùng, vì vậy đẻ sử dụng animation trong react thường có nhiều th ư viện để hỗ trợ 1 trong các thư viện phổ biến nhất là react Spring
- * 
+ *
  * - Các hooks cơ bản
  *  + useSpring
  *  + useSprings
  *  + useTrail
  *  + usetransition
  *  + useChain\
- * 
- * Các bước tìm hiểu một cái thư viện là 
+ *
+ * Các bước tìm hiểu một cái thư viện là
  * B1: Cài đặt thư viện
- * B2: Chép demo chạy thử làm theo hướng dẫn document 
+ * B2: Chép demo chạy thử làm theo hướng dẫn document
  * B3: Đọc tài liệu về các thuộc tính giá trị để hiểu demo đó
  * B4: Chỉnh sửa lại theo 1 ví dụ nào đó của mình
  */
@@ -25,3 +25,4 @@
 // - useSpring hỗ trợ hàm chuyển động từ trạng thái a -> b
 // - Ta có thể tùy biến tên biến, ví dụ bên dưới opacity(có thể đặt tên lại! không phải tên css đâu chẳng qua ta muốn gắn nó vào style cho tiện nên mới đặt tên trùng)
 //
+// Trong biến có thể truyền linh động, ví dụ như là mảng, object không nhất thiêt là phải truyền vào chuỗi không.
