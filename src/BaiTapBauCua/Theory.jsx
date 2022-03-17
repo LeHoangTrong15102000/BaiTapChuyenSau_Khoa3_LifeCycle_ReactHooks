378291c0c0352d1d64739303652623e5ff39787d
Le Hoang Trong <langtupro0456@gmail.com>
2022-03-17 15:22:15 +0700

    Study of BaiTapBauCuaCaCop

---
+++ src/BaiTapBauCua/Theory.jsx
@@ -0,0 +1,13 @@
+// Các quy trình thực hiện bai tập BauCuaCaCop CyberSoft
+
+// Học cybersoft để tích lũy hướng giải quyết vấn đề, khi mà gặp những vấn đề khó thì có thể giải quyết được
+/**
+ * Những yêu cầu cần thực hiện trong bài BauCuaCaCop CyberSoft
+ *      + Dàn layout cho game
+ *      + Thiết kế Redux lưu trữ state
+ *      + Xây dựng chức năng cho game
+ *      + tạo hiệu ứng animation sử dụng react Spring
+ * 
+ * Note: Để làm một bài hay một dự án thì cần phải hiểu nghiệp vụ của nó như thế nào rồi mới triển khai ý tưởng để làm
+ *     - Khi mà có Redux thì chia bao nhiêu component thì cũng không quan trọng , nhưng mà phải chia component sao cho hợp lý
+ */
