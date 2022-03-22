70a7504392efb2b020bd0c15aa142c461ebdec85
Le Hoang Trong <langtupro0456@gmail.com>
2022-03-22 10:34:22 +0700

    Add file in BaiTapBauCua

--- src/redux/actions/BaiTapGameBauCuaAction.jsx
+++ src/redux/actions/BaiTapGameBauCuaAction.jsx
@@ -4,7 +4,6 @@ import {
   XOC_BAU_CUA,
 } from '../types/BaiTapGameBauCuaTypes';
 
-
 // cái tên biến ở đây truyên lên phải cần phải nhớ để khi vào Reducer còn gọi ra cho giống
 export const datCuocAction = (quanCuoc, tangGiam) => ({
   type: DAT_CUOC_BAU_CUA,
